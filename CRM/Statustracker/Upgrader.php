d486b7101f5ce9b78cc6b1b37e30830761f83e59
B. Endres <endres@systopia.de>
2019-05-31 17:34:36 +0200

    create data structure

---
+++ CRM/Statustracker/Upgrader.php
@@ -0,0 +1,37 @@
+<?php
+/*-------------------------------------------------------+
+| SYSTOPIA Status Tracker Extension                      |
+| Copyright (C) 2019 SYSTOPIA                            |
+| Author: B. Endres (endres@systopia.de)                 |
+| http://www.systopia.de/                                |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+use CRM_Statustracker_ExtensionUtil as E;
+
+/**
+ * Collection of upgrade steps.
+ */
+class CRM_Statustracker_Upgrader extends CRM_Statustracker_Upgrader_Base {
+
+  /**
+   * Installer
+   */
+  public function install() {
+    // run the custom group sync
+    require_once 'CRM/Statustracker/CustomData.php';
+    $customData = new CRM_Statustracker_CustomData('de.systopia.statustracker');
+    $customData->syncOptionGroup(__DIR__ . '/../../resources/status_tracker_category_option_group.json');
+    $customData->syncOptionGroup(__DIR__ . '/../../resources/status_tracker_status_option_group.json');
+    $customData->syncCustomGroup(__DIR__ . '/../../resources/status_tracker_custom_group.json');
+
+    return TRUE;
+  }
+}
--- statustracker.php
+++ statustracker.php
@@ -1,4 +1,18 @@
 <?php
+/*-------------------------------------------------------+
+| SYSTOPIA Status Tracker Extension                      |
+| Copyright (C) 2019 SYSTOPIA                            |
+| Author: B. Endres (endres@systopia.de)                 |
+| http://www.systopia.de/                                |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
 
 require_once 'statustracker.civix.php';
 use CRM_Statustracker_ExtensionUtil as E;
